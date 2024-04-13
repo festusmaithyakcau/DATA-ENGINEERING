a05886417f308a33758d45cb24072ab1ae8ac142
Festus Maithya <festusmaithya264@gmail.com>
2024-04-13 22:22:43 +0300

    Update external_connection.py
    Adds code snippet for configuring alternate authentication for external connection to Fabric using Spark

--- MS FABRIC/SPARK/external_connection.py
+++ MS FABRIC/SPARK/external_connection.py
@@ -16,3 +16,36 @@ blob_df = spark.read.parquet(wasbs_path)
 
 # Show the Azure Blob DataFrame
 blob_df.show()
+
+
+
+# Configure alternate authentication
+"""
+The previous code snippet connects to the source and reads the data into a DataFrame. Depending on your source, you may need a different authentication type, such as Service Principal, OAuth, etc. Here's an example connecting to an Azure SQL Database with a Service Principal:
+"""
+# Placeholders for Azure SQL Database connection info
+server_name = "your_server_name.database.windows.net"
+port_number = 1433  # Default port number for SQL Server
+database_name = "your_database_name"
+table_name = "YourTableName" # Database table
+client_id = "YOUR_CLIENT_ID"  # Service principal client ID
+client_secret = "YOUR_CLIENT_SECRET"  # Service principal client secret
+tenant_id = "YOUR_TENANT_ID"  # Azure Active Directory tenant ID
+
+
+# Build the Azure SQL Database JDBC URL with Service Principal (Active Directory Integrated)
+jdbc_url = f"jdbc:sqlserver://{server_name}:{port_number};database={database_name};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;Authentication=ActiveDirectoryIntegrated"
+
+# Properties for the JDBC connection
+properties = {
+    "user": client_id, 
+    "password": client_secret,  
+    "driver": "com.microsoft.sqlserver.jdbc.SQLServerDriver",
+    "tenantId": tenant_id  
+}
+
+# Read entire table from Azure SQL Database using AAD Integrated authentication
+sql_df = spark.read.jdbc(url=jdbc_url, table=table_name, properties=properties)
+
+# Show the Azure SQL DataFrame
+sql_df.show()
